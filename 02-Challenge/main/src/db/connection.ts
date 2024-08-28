c1a13ceac7b3bef787b3fdaf8af72323eb1aac8f
haydencfb <haydenfitzbrintle@gmail.com>
2024-08-27 21:23:32 -0700

    Work Done in Class Review
    this commit handles the updates that were made during our review class that was held on August 27th, 2024

--- 02-Challenge/main/src/db/connection.ts
+++ 02-Challenge/main/src/db/connection.ts
@@ -6,7 +6,6 @@ const { Pool } = pg;
 
 const pool = new Pool({
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
     host: 'localhost',
     database: process.env.DB_NAME,
     port: 5432
